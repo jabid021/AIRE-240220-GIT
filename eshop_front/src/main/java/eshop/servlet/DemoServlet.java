0d27af2f1b5a2661f80262e4ef7f36791c79f18e
Jordan <Jordanabid@gmail.com>
2024-04-03 17:41:11 +0200

    Servlet v1 (c'est bien nul...)

---
+++ eshop_front/src/main/java/eshop/servlet/DemoServlet.java
@@ -0,0 +1,49 @@
+package eshop.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import eshop.context.Singleton;
+import eshop.model.Produit;
+
+@WebServlet("/demo.jsp")
+public class DemoServlet extends HttpServlet {
+
+ //Gestion des pages web en java : 
+	//v1 : on fait du java et on ajoute du html dedans
+	//v2 : on fait du html et on ajoute du java dedans
+	//v3 : ????? MVC
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		List<Produit> produits = Singleton.getInstance().getDaoProduit().findAll();
+		
+		response.getWriter().println("<body>");
+		response.getWriter().println("<h1>Liste des produits</h1>");
+		response.getWriter().println("<table>");
+		response.getWriter().println("<tr><th>ID</th><th>Libelle</th><th>Prix</th></tr>");
+		for(Produit p : produits) 
+		{
+			response.getWriter().println("<tr><td>"+p.getId()+"</td><td>"+p.getLibelle()+"</td><td>"+p.getPrix()+"</td></tr>");
+		}
+		response.getWriter().println("</table>");
+		
+		response.getWriter().println("</body>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
