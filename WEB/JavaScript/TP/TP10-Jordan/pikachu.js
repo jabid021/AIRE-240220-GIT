3698ebc3cfee0a2f7d26b433c4d60d28f12fb242
Jordan <Jordanabid@gmail.com>
2024-04-03 15:55:45 +0200

    Correction pikachu tp10

--- WEB/JavaScript/TP/TP10-Jordan/pikachu.js
+++ WEB/JavaScript/TP/TP10-Jordan/pikachu.js
@@ -12,29 +12,71 @@ var direction="Down";
 imgPikachu.setAttribute("src","assets/img/"+pokemon+direction+".png");
 
 
+inputName.onkeyup=verifInput;
+btnStart.onclick=partie;
+btnMusique.onclick=function(){themePokemon.pause();};
+function verifInput(e)
+{
+  if(inputName.value=="")
+  {
+    btnStart.disabled=true;
+  }
+  else
+  {
+    btnStart.disabled=false;
+    if(e.key=="Enter")
+    {
+      partie();
+    }
+  }
+}
+
+function partie()
+{
+  btnMusique.style.display="block";
+  themePokemon.play();
+
+formStart.style.display="none";
+grass.style.display="block";
+pikachu.setAttribute("title",inputName.value);
+
+document.body.onkeydown=deplacement;
+}
+
 
 function deplacement(event)
 {
   if(event.key=="ArrowDown" || event.key=="s" )
   {
-
+    if(posY<=630){
+      posY+=mouvement;
+    }
+    direction="Down";
   }
   else if(event.key=="ArrowRight" || event.key=="d" )
   {
-
+    if(posX<=630){
+      posX+=mouvement;
+    }
+    direction="Right";
   }
 
   else if(event.key=="ArrowLeft" || event.key=="q")
   {
-
+    if(posX>=30){
+      posX-=mouvement;
+    }
+    direction="Left";
   }
 
   else if(event.key=="ArrowUp" || event.key=="z")
   {
-
+    if(posY>=30){
+      posY-=mouvement;
+    }
+    direction="Up";
   }
   pikachu.style.top=posY+"px";
   pikachu.style.left=posX+"px";
   imgPikachu.setAttribute("src","assets/img/"+pokemon+direction+".png");
-
 }
