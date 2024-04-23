c90c94a42ae205e71e2420b5372abb697b97db43
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2024-04-23 16:36:16 +0200

    tp-angular-v8

--- tp-angular/src/app/app.module.ts
+++ tp-angular/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BackgroundBlackDirective } from './background-black.directive';
 import { TodoStatePipe } from './todo-state.pipe';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BackgroundBlackDirective,
-    TodoStatePipe
+    TodoStatePipe,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
---
+++ tp-angular/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hello-world, [hello-world]',
+  templateUrl: './hello-world.component.html',
+  styleUrl: './hello-world.component.css'
+})
+export class HelloWorldComponent {
+
+
+
+
+}
