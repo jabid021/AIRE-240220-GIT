6611a06688cea3c798fd3c1764f4710659e3104d
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2024-04-25 15:24:52 +0200

    tp-angular-todo-v5
    TP affichage formulaire partie 2 + TP save() + remove() 1ère partie

--- tp-angular-todo/src/app/utilisateur/utilisateur.component.ts
+++ tp-angular-todo/src/app/utilisateur/utilisateur.component.ts
@@ -9,6 +9,8 @@ import { Utilisateur } from '../model';
 export class UtilisateurComponent {
   utilisateurs: Array<Utilisateur> = new Array<Utilisateur>();
 
+  utilisateurForm?: Utilisateur = undefined;
+
   constructor() {
     this.utilisateurs.push(new Utilisateur(2, "AZID", "Hana", "hazid", "123456", false));
     this.utilisateurs.push(new Utilisateur(5, "BARDOU", "Hedieh", "hbardou", "123456", false));
@@ -17,14 +19,27 @@ export class UtilisateurComponent {
   }
 
   edit(id?: number) {
-
+    this.utilisateurForm = {...this.utilisateurs.find(u => u.id == id)};
   }
 
   add() {
+    this.utilisateurForm = new Utilisateur();
+  }
 
+  save() {
+    // elle va utiliser les informations synchronisées depuis le formulaire vers la variable utilisateurForm
+    // si l'id est renseigné (modification), vous devez écraser la cellule (position) du tableau qui correspondent aux mêmes ID
+    
+    // sinon (création), vous devez rechercher l'id le plus grand du tableau afin d'affecter l'incrément de celui-ci dans le nouvel objet utilisateurForm
+    // puis le rajouter dans le tableau
   }
 
   cancel() {
-    
+    this.utilisateurForm = undefined;
+  }
+
+  remove(id?: number) {
+    // rechercher par l'id, la position de l'objet à supprimer 
+    // effectuer la suppression sur le tableau avec la méthodse splice
   }
 }
