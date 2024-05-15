88ffa117b0a48ca42b5c17ae0bb6163988495dce
Jeremy <jeremy.perrouault@ascadis.fr>
2024-05-15 13:03:49 +0200

    JOUR 3 - IMPLEMENTATION KAFKA - DEMO

--- MICROSERVICES/produit-service/src/main/java/fr/formation/ProduitServiceApplication.java
+++ MICROSERVICES/produit-service/src/main/java/fr/formation/ProduitServiceApplication.java
@@ -1,13 +1,33 @@
 package fr.formation;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 public class ProduitServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProduitServiceApplication.class, args);
+    }
+
+    // @Bean
+    Consumer<String> onMessageDemo() {
+        return evt -> {
+            System.out.println(evt);
+        };
+    }
+
+    @Bean
+    Function<String, String> onMessageDemoUpperCase() {
+        return evt -> {
+            System.out.println("UPPERCASE " + evt);
+
+            return evt.toUpperCase();
+        };
     }
 }
