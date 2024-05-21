3493927bd8b21cb0b9a2653ce16b7ab31b644af8
Jeremy <jeremy.perrouault@ascadis.fr>
2024-05-21 15:40:34 +0200

    JOUR 1 - SMALL DATA JDBC BATCH

--- BIG-DATA/small-data/src/main/java/fr/formation/api/ProduitApiController.java
+++ BIG-DATA/small-data/src/main/java/fr/formation/api/ProduitApiController.java
@@ -1,6 +1,9 @@
 package fr.formation.api;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,5 +36,38 @@ public class ProduitApiController {
         }
 
         this.repository.saveAll(produits);
+    }
+
+    @PostMapping("/jdbc")
+    public void generateJdbc() {
+        // Récupérer une connexion JDBC
+
+        // Syntaxe try-with-resources : C'est Java qui va appeler la méthode close() de la connection
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/small_data", "root", "root")) {
+            connection.setAutoCommit(false);
+
+            // Récupérer un Statement pour exécuter la requête (un PreparedStatement est encore mieux !)
+            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO produit (name, price) VALUES (?, ?)")) {
+                for (int i = 0; i < 2_000; i++) {
+                    Produit produit = new Produit();
+        
+                    produit.setName("Produit " + i);
+                    produit.setPrice(new BigDecimal(i));
+        
+                    statement.setString(1, produit.getName());
+                    statement.setBigDecimal(2, produit.getPrice());
+        
+                    statement.addBatch();
+                }
+
+                statement.executeBatch();
+                connection.commit();
+            }
+        }
+
+        catch (Exception ex) {
+            System.out.println("ERREUR");
+        }
+
     }
 }
