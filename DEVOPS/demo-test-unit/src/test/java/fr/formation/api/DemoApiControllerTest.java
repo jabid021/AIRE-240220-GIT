8460fe43fa096ad0033e03426af7677d3222f3fc
Jeremy <jeremy.perrouault@ascadis.fr>
2024-05-28 12:16:16 +0200

    JOUR 1 - TEST MOCK MVC

---
+++ DEVOPS/demo-test-unit/src/test/java/fr/formation/api/DemoApiControllerTest.java
@@ -0,0 +1,26 @@
+package fr.formation.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc // Prépare le MockMvc pour le test
+class DemoApiControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    void shouldHelloDemoStatusOk() throws Exception {
+        // given
+
+        // when
+        ResultActions result = this.mockMvc.perform(MockMvcRequestBuilders.get("/api/demo"));
+
+        // then
+        result.andExpect(MockMvcResultMatchers.status().isOk());
+    }
+}
