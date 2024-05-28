3209dbd7c4fe96f5a37a664eebaa967b60d893c0
Jeremy <jeremy.perrouault@ascadis.fr>
2024-05-28 12:16:33 +0200

    JOUR 1 - TEST MOCK MVC

--- DEVOPS/demo-test-unit/src/test/java/fr/formation/api/DemoApiControllerTest.java
+++ DEVOPS/demo-test-unit/src/test/java/fr/formation/api/DemoApiControllerTest.java
@@ -1,5 +1,6 @@
 package fr.formation.api;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,7 @@ class DemoApiControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Test
     void shouldHelloDemoStatusOk() throws Exception {
         // given
 
